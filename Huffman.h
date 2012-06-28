d7409553588de4e550d60d9a370d12bd7fb7d346
Andreas Moe <moe.andreas@gmail.com>
2012-06-28 12:09:05 +0300

    Working on new structure...

--- Huffman.h
+++ Huffman.h
@@ -10,6 +10,71 @@
 // ---------- HUFFMAN CLASS -------------------------------------------------------------
 class Huffman {
 private:
+
+    /*
+    
+    struct DataFile {
+        char    magicNumber;
+        char    paddingOffset;
+        char*   data;
+    };
+    
+    struct TableEntry{
+        char    data;
+        char    size;
+        int     bits;
+    };
+    
+    struct TableFile {
+        char    magicNumber;
+        int     tableSize;
+        std::vector<TableEntry> data;
+    };
+    
+    Node*                               tree;
+    std::string                         decodedResult;
+    std::vector<int>                    encodedResult;
+    std::map<char, std::pair<int, int>> encodingMap;
+    std::map<char, double>*             frequencies;
+    
+    std::map<char, double>*     getFrequencies(std::string s);
+    void                        Sort(std::vector<Node*>& v);
+    void                        buildTree();
+    void                        showTree();
+    void                        showIntVector(std::vector<int>& v);
+    std::vector<int>            buildEncodedResult(std::string s);
+    std::string                 buildDecodedResult(std::map<char, std::pair<int, int>>& m,
+                                                   int size, char* data);
+                                                   
+    DataFile*   createDataFile(std::vector<int>& v);
+    TableFile*  createTableFile(std::map<char, std::pair<int, int>>& m);
+    
+    DataFile*   readDataFile(ifstream* file);
+    TableFile*  readTableFile(ifstream* file);
+    
+    void        writeDataFile(ofstream* file);
+    void        writeDataFile(ofstream* file);
+    
+    */
+    
+    /*
+    
+    DATAFILE:
+    BYTE    0xHF
+    BYTE    PADDING OFFSET
+    CHAR*   DATA
+    
+    TABLEFILE:
+    BYTE    0xHF
+    
+        per entry:
+        CHAR ch
+        CHAR paddingOffset
+        int  data
+    
+    
+    */
+
     Node*                               tree;
     std::string                         decodedResult;
     std::vector<bool>                   encodedResult;
@@ -24,6 +89,14 @@ private:
 public:
     Huffman();
     ~Huffman();
+    
+    /*
+    
+    void encode(ifstream* file);
+    void decode(ifstream* table, ifstream* data)
+    
+    */
+    
     void encode(std::string s);
     void decode(std::string s);
 };
