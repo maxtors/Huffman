da2c02c2844d954b5c91d42c97902f7362ace140
Andreas Moe <moe.andreas@gmail.com>
2012-06-29 10:22:38 +0300

    Fixed error handeling

--- main.cpp
+++ main.cpp
@@ -10,35 +10,37 @@ int main(int argc, char* argv[]) {
 
     try {
         if (argc != 3 && argc != 4) throw "Invalid number of arguments";
-        if (strcmp(argv[1],"-e")!=0 &&
-            strcmp(argv[1],"-d")!=0) throw "Incorrect arguemnt";
-        if (strcmp(argv[1],"-e")==0 && argc != 3) throw "Invalid number of arguments";
-        if (strcmp(argv[1],"-d")==0 && argc != 4) throw "Invalid number of arguments";
 
         if (strcmp(argv[1],"-e")==0) {
+            if (argc != 3) throw "Innvalid number of arguments";
             file.open(argv[2]);
             if (!file) throw "Could not open file";
             file.close();
 
+            // Encode the wanted file
             h.encode(argv[2]);
         }
         else if (strcmp(argv[1],"-d")==0) {
+            if (argc != 4) throw "Innvalid number of arguments";
             file.open(argv[2]);
             if (!file) throw "Could not open datafile";
             file.close();
             file.open(argv[2]);
             if (!file) throw "Could not open tablefile";
             file.close();
-
+            
+            // Decode the wanted files
             h.decode(argv[2], argv[3]);
+        }
+        else {
+            throw "Incorrect arguments";
         }
     }
     catch (char* err) {
         if (file.is_open()) file.close();
-        std::cout << "\n\n" << err << ".\n\n"
-                  << "\t-Encoding: -e filename.type\n"
-                  << "\t-Decoding: -d file1.datafile file2.tablefile\n";
+        std::cout << "\n\n" << err << ".\n\n";
+        std::cout << "\t-Encoding: -e filename.type\n";
+        std::cout << "\t-Decoding: -d file1.datafile file2.tablefile\n";
     }
-
     return 0;
 }
